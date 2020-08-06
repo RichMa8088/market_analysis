a1de7ba017289ae76346bc77c75ca682edefb88b
Rich8088 <maxma8088@126.coom>
2020-08-06 16:23:23 +0800

    0.01

---
+++ data_into_DB.py
@@ -0,0 +1,85 @@
+# -*- coding:utf-8 -*-
+# ----------------------------------------------------------------------------#
+# --------------------0、加载模块及初始化参数--------#
+# --加载常规模块
+from time import time
+import datetime
+import pandas as pd
+# --加载其他模块
+from read_csv_file import read_csv_file
+from sqlalchemy.dialects.mysql import BIGINT, BOOLEAN, CHAR, DATE, DATETIME, DECIMAL, DOUBLE, \
+    FLOAT, INTEGER, MEDIUMINT, SMALLINT, TIME, TIMESTAMP, TINYINT, VARCHAR, YEAR  # mysql类型
+from sqlalchemy import create_engine  # 引擎
+from sqlalchemy.ext.declarative import declarative_base  # 基类
+
+# --参数设置
+start_time = time()
+pd.set_option('display.max_rows', 20)
+pd.set_option('display.max_columns', 30)
+pd.set_option('display.width', 500)
+missing_values = ['n/a', 'na', '--', 'Null', 'NULL', '\t']
+rootDir = '/home/rich/myfile/data/1行业相关数据/洗护清洁剂_卫生巾_纸_香薰/类目趋势'  # 输入根目录的路径
+
+
+# --数据库类型字典
+def mapping_df_types(df):
+    type_dict = {}
+    for i, j in zip(df.columns, df.dtypes):
+        if "object" in str(j):
+            type_dict.update({i: VARCHAR(length=512)})
+        if "float" in str(j):
+            type_dict.update({i: DECIMAL(19, 2)})
+        if "int" in str(j):
+            type_dict.update({i: INTEGER()})
+    return type_dict
+
+
+# --sqlalchemy基本操作
+HOSTNAME = "127.0.0.1"
+PORT = "3306"
+DATABASE = "market_analysis"
+ACCOUNT = "root"
+PASSWORD = "123456"
+DB_URI = "mysql+pymysql://{}:{}@{}:{}/{}?charset=UTF8MB4" \
+    .format(ACCOUNT, PASSWORD, HOSTNAME, PORT, DATABASE)
+engine = create_engine(DB_URI, pool_recycle=3600)
+conn = engine.connect()  # --连接数据库
+Base = declarative_base()  # --基类
+
+# ----------------------------------------------------------------------------#
+# --------------------1、类目趋势数据读取并且处理--------#
+df_cg_trends = read_csv_file(rootDir, '行业趋势', missing_values, 0, coding='utf-8')
+df_cg_trends = df_cg_trends.rename(
+    columns=lambda x: x.replace("'", "").replace('"', '').replace(" ", ""))
+# --字段处理
+df_cg_trends.drop(['终端', '类别', '客单价', '路径', '文件名'], axis=1, inplace=True)
+
+# 格式转化
+df_cg_trends['月份'] = pd.to_datetime(df_cg_trends['月份'])
+#
+df_cg_trends['搜索人数'] = df_cg_trends['搜索人数'].astype(int)
+df_cg_trends['搜索次数'] = df_cg_trends['搜索次数'].astype(int)
+df_cg_trends['访客数'] = df_cg_trends['访客数'].astype(int)
+df_cg_trends['浏览量'] = df_cg_trends['浏览量'].astype(int)
+df_cg_trends['收藏人数'] = df_cg_trends['收藏人数'].astype(int)
+df_cg_trends['收藏次数'] = df_cg_trends['收藏次数'].astype(int)
+df_cg_trends['加购人数'] = df_cg_trends['加购人数'].astype(int)
+df_cg_trends['加购次数'] = df_cg_trends['加购次数'].astype(int)
+df_cg_trends['支付人数'] = df_cg_trends['支付人数'].astype(int)
+#
+df_cg_trends['交易金额'] = df_cg_trends['交易金额'].astype(float)
+# 保留最新的数据记录,去重复
+df_cg_trends.sort_values(by=['类目名', '月份'], ascending=True, inplace=True)
+df_cg_trends = df_cg_trends.reset_index(drop=True)
+df_cg_trends = df_cg_trends.drop_duplicates(subset=['类目名', '月份'], keep='last')
+# print(df_cg_trends.info())
+# df_cg_trends.to_csv('/home/rich/myfile/output/temp.csv')
+
+
+# ----------------------------------------------------------------------------#
+# ----------------------将数据导入数据库------#
+df_cg_trends.to_sql(name='category_trends', con=engine, if_exists='append', index=False,
+                    dtype=mapping_df_types(df_cg_trends))
+# ----------------------------------------------------------------------------#
+end_time = time()  # 计时结束
+print('运行时长： %f' % (end_time - start_time))  # 打印运行时长
---
+++ read_csv_file.py
@@ -0,0 +1,26 @@
+# -*- coding:utf-8 -*-
+# -----------------------------------0、加载模块及初始化参数---------------------------------------#
+# --加载常规模块
+import os  # 文件路径
+import pandas as pd
+
+
+# --读取csv文件，根据关键字进行筛选，将选中的数据放入一个df
+def read_csv_file(file_dir, file_name_key, missing_values, header_rows, coding):
+    df_csv = pd.DataFrame()
+    for parent, DirNames, FileNames in os.walk(file_dir):
+        for FileName in FileNames:
+            if FileName.__contains__('.csv'):
+                if FileName.__contains__(file_name_key):
+                    csv1 = pd.read_csv(os.path.join(parent, FileName), sep=',', index_col=None,
+                                       encoding=coding,
+                                       dtype=object, header=header_rows, na_values=missing_values)
+                    csv1['路径'] = parent
+                    csv1['文件名'] = FileName
+                    df_csv = df_csv.append(csv1)
+                else:
+                    continue
+            else:
+                continue
+    df_csv.dropna(axis=0, how='all', inplace=True)
+    return df_csv
